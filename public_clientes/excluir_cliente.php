a9a8cd829d4772f64e3690b7fbff87d205be6e48
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-08-25 16:29:41 -0300

    add - implementei a funcionalidade de exclusão de clientes

--- public_clientes/excluir_cliente.php
+++ public_clientes/excluir_cliente.php
@@ -2,6 +2,22 @@
 
 require_once "../infra/conexao.php";
 
+$id = $_GET["id"];
 
+$sql = "DELETE FROM clientes WHERE id = ?";
+
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+
+    header("Location: listar_cliente.php");
+    exit;
+
+} else {
+
+    echo "Erro ao excluir cliente.";
+
+}
 
 ?>
