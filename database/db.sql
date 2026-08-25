7453ca8ca6d97193100d8e598f25ff19dbea4b9c
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-08-25 11:21:07 -0300

    add - criei a tabela clientes no banco de dados petshop_db

--- database/db.sql
+++ database/db.sql
@@ -1,2 +1,18 @@
 create database if not exists petshop_db;
 use petshop_db;
+
+CREATE TABLE clientes ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL 
+); 
+CREATE TABLE animais ( 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL, 
+    especie VARCHAR(50) NOT NULL, 
+    raca VARCHAR(100) NOT NULL, 
+    idade INT NOT NULL, 
+    cliente_id INT NOT NULL, 
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id) 
+);
