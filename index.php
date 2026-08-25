de03804eeb0e3a94e0445e1f1c5839b0d2061367
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-08-25 16:31:29 -0300

    add - fiz a pagina inicial que é o index.php

--- index.php
+++ index.php
@@ -1,21 +1,44 @@
-<?php
-include "infra/conexao.php";
-
-
-
-?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
+
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Pet Shop AUmigos</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/style.css">
+
 </head>
+
 <body>
-    
+
+<div class="container mt-4">
+
+    <h1>Pet Shop AUmigos</h1>
+
+    <p>Sistema de cadastro de clientes e animais.</p>
+
+    <a href="clientes/cadastrar_cliente.php" class="btn btn-primary">
+        Cadastrar Cliente
+    </a>
+
+    <a href="clientes/listar_cliente.php" class="btn btn-secondary">
+        Ver Clientes
+    </a>
+
+    <a href="animais/cadastrar_animal.php" class="btn btn-primary">
+        Cadastrar Animal
+    </a>
+
+    <a href="animais/listar_animais.php" class="btn btn-secondary">
+        Ver Animais
+    </a>
+
+</div>
+
 </body>
 </html>
