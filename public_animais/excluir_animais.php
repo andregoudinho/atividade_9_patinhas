33251581ceb67046a9e7d3ee5a4c89f9fc4bd798
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-08-25 19:25:57 -0300

    add - implementei a funcionalidade de exclusão de animais

--- public_animais/excluir_animais.php
+++ public_animais/excluir_animais.php
@@ -2,6 +2,22 @@
 
 require_once "../infra/conexao.php";
 
+$id = $_GET["id"];
 
+$sql = "DELETE FROM animais WHERE id = ?";
+
+$stmt = $conexao->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+
+    header("Location: listar_animais.php");
+    exit;
+
+} else {
+
+    echo "Erro ao excluir animal.";
+
+}
 
 ?>
