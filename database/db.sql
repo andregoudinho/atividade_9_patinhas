37620eff5fa0581dddfacd5de301c26486a0a777
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-08-26 11:39:43 -0300

    refactor - removi os campos de email e telefone da tabela de clientes

--- database/db.sql
+++ database/db.sql
@@ -3,9 +3,7 @@ USE petshop_db;
 
 CREATE TABLE clientes ( 
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL 
+    nome VARCHAR(100) NOT NULL
 ); 
 
 CREATE TABLE animais ( 
